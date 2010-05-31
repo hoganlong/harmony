7d3b83841a669aeaab8bc3847320858815648516
hoganlong <hoganlong@gmail.com>
2010-05-30 21:24:01 -0400

    Format stuff and remove un-used code.

--- src/js/harmony.js
+++ src/js/harmony.js
@@ -1,11 +1,4 @@
-
-
 (function(window,document,undefined){
-
-
-
-
-
 
     function ribbon( context )
     {
@@ -90,7 +83,7 @@
     			this.context.stroke();
     		}
     	}
-    }
+    }  /* end ribbon */
 
     function bargs( _fn )
     {
@@ -102,74 +95,45 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-    var i, brush, BRUSHES = ["ribbon"],
-    COLOR = [0, 0, 0], BACKGROUND_COLOR = [250, 250, 250],
-    SCREEN_WIDTH = window.innerWidth,
-    SCREEN_HEIGHT = window.innerHeight,
-    container, foregroundColorSelector, backgroundColorSelector, menu, about,
-    canvas, flattenCanvas, context,
-    isForegroundColorSelectorVisible = false, isBackgroundColorSelectorVisible = false, isAboutVisible = false,
-    isMenuMouseOver = false, shiftKeyIsDown = false, altKeyIsDown = false;
-
-    
-
+var i, brush, BRUSHES = ["ribbon"],
+  COLOR = [0, 0, 0], BACKGROUND_COLOR = [250, 250, 250],
+  SCREEN_WIDTH = window.innerWidth,
+  SCREEN_HEIGHT = window.innerHeight,
+  container, foregroundColorSelector, backgroundColorSelector, menu, about,
+  canvas, flattenCanvas, context,
+  isForegroundColorSelectorVisible = false, isBackgroundColorSelectorVisible = false, isAboutVisible = false,
+  isMenuMouseOver = false, shiftKeyIsDown = false, altKeyIsDown = false;
 
 window.harmony = function init()
 {
-	var hash, palette;
+   var hash, palette;
 	
-	//document.body.style.backgroundColor = 'rgb(' + BACKGROUND_COLOR[0] + ', ' + BACKGROUND_COLOR[1] + ', ' + BACKGROUND_COLOR[2] + ')';
-
-	container = document.createElement('div');
-	document.body.appendChild(container);
+   container = document.createElement('div');
+   document.body.appendChild(container);
 	
-	canvas = document.createElement("canvas");
-	canvas.width = SCREEN_WIDTH;
-	canvas.height = SCREEN_HEIGHT;
-	canvas.style.cursor = 'crosshair';
-	container.appendChild(canvas);
+   canvas = document.createElement("canvas");
+   canvas.width = SCREEN_WIDTH;
+   canvas.height = SCREEN_HEIGHT;
+   canvas.style.cursor = 'crosshair';
+   container.appendChild(canvas);
 	
-	if (!canvas.getContext) return;
+   if (!canvas.getContext) return;
 	
-	context = canvas.getContext("2d");
+   context = canvas.getContext("2d");
 	
-	flattenCanvas = document.createElement("canvas");
-	flattenCanvas.width = SCREEN_WIDTH;
-	flattenCanvas.height = SCREEN_HEIGHT;
-
+   flattenCanvas = document.createElement("canvas");
+   flattenCanvas.width = SCREEN_WIDTH;
+   flattenCanvas.height = SCREEN_HEIGHT;
 	
-
-
+   window.addEventListener('mousemove', onWindowMouseMove, false);
+   window.addEventListener('resize', onWindowResize, false);
 	
-        /* don't need this -- we do it in the resize
-	if (!brush)
-	{
-		brush = new ribbon(context);
-	}
-        */
+   document.addEventListener('mouseout', onCanvasMouseUp, false);
 	
-
-	window.addEventListener('mousemove', onWindowMouseMove, false);
-	window.addEventListener('resize', onWindowResize, false);
-	window.addEventListener('keydown', onDocumentKeyDown, false);
-	window.addEventListener('keyup', onDocumentKeyUp, false);
+   canvas.addEventListener('mousemove', onCanvasMouseMove, false);
+  // canvas.addEventListener('touchstart', onCanvasTouchStart, false);
 	
-	document.addEventListener('mouseout', onCanvasMouseUp, false);
-	
-	canvas.addEventListener('mousemove', onCanvasMouseMove, false);
-	canvas.addEventListener('touchstart', onCanvasTouchStart, false);
-	
-	onWindowResize(null);
+   onWindowResize(null);
 }
 
 
@@ -204,42 +168,6 @@ function onWindowResize()
 }
 
 
-// DOCUMENT
-
-function onDocumentMouseDown( event )
-{
-	if (!isMenuMouseOver)
-		event.preventDefault();
-}
-
-function onDocumentKeyDown( event )
-{
-	if (shiftKeyIsDown)
-		return;
-		
-	switch(event.keyCode)
-	{
-
-		case 18: // Alt
-			altKeyIsDown = true;
-			break;
-	}
-}
-
-function onDocumentKeyUp( event )
-{
-	switch(event.keyCode)
-	{
-		case 16: // Shift
-			shiftKeyIsDown = false;
-			foregroundColorSelector.container.style.visibility = 'hidden';			
-			break;
-		case 18: // Alt
-			altKeyIsDown = false;
-			break;	
-	}
-}
-
 // COLOR SELECTORS
 
 function setForegroundColor( x, y )
@@ -247,61 +175,6 @@ function setForegroundColor( x, y )
 	foregroundColorSelector.update( x, y );
 	COLOR = foregroundColorSelector.getColor();
 	menu.setForegroundColor( COLOR );	
-}
-
-function onForegroundColorSelectorMouseDown( event )
-{
-	window.addEventListener('mousemove', onForegroundColorSelectorMouseMove, false);
-	window.addEventListener('mouseup', onForegroundColorSelectorMouseUp, false);
-	
-	setForegroundColor( event.clientX - foregroundColorSelector.container.offsetLeft, event.clientY - foregroundColorSelector.container.offsetTop );	
-}
-
-function onForegroundColorSelectorMouseMove( event )
-{
-	setForegroundColor( event.clientX - foregroundColorSelector.container.offsetLeft, event.clientY - foregroundColorSelector.container.offsetTop );
-}
-
-function onForegroundColorSelectorMouseUp( event )
-{
-	window.removeEventListener('mousemove', onForegroundColorSelectorMouseMove, false);
-	window.removeEventListener('mouseup', onForegroundColorSelectorMouseUp, false);
-
-	setForegroundColor( event.clientX - foregroundColorSelector.container.offsetLeft, event.clientY - foregroundColorSelector.container.offsetTop );
-}
-
-function onForegroundColorSelectorTouchStart( event )
-{
-	if(event.touches.length == 1)
-	{
-		event.preventDefault();
-		
-		setForegroundColor( event.touches[0].pageX - foregroundColorSelector.container.offsetLeft, event.touches[0].pageY - foregroundColorSelector.container.offsetTop );
-		
-		window.addEventListener('touchmove', onForegroundColorSelectorTouchMove, false);
-		window.addEventListener('touchend', onForegroundColorSelectorTouchEnd, false);
-	}
-}
-
-function onForegroundColorSelectorTouchMove( event )
-{
-	if(event.touches.length == 1)
-	{
-		event.preventDefault();
-		
-		setForegroundColor( event.touches[0].pageX - foregroundColorSelector.container.offsetLeft, event.touches[0].pageY - foregroundColorSelector.container.offsetTop );
-	}
-}
-
-function onForegroundColorSelectorTouchEnd( event )
-{
-	if(event.touches.length == 0)
-	{
-		event.preventDefault();
-		
-		window.removeEventListener('touchmove', onForegroundColorSelectorTouchMove, false);
-		window.removeEventListener('touchend', onForegroundColorSelectorTouchEnd, false);
-	}	
 }
 
 
@@ -316,137 +189,7 @@ function setBackgroundColor( x, y )
 	document.body.style.backgroundColor = 'rgb(' + BACKGROUND_COLOR[0] + ', ' + BACKGROUND_COLOR[1] + ', ' + BACKGROUND_COLOR[2] + ')';	
 }
 
-function onBackgroundColorSelectorMouseDown( event )
-{
-	window.addEventListener('mousemove', onBackgroundColorSelectorMouseMove, false);
-	window.addEventListener('mouseup', onBackgroundColorSelectorMouseUp, false);
-}
-
-function onBackgroundColorSelectorMouseMove( event )
-{
-	setBackgroundColor( event.clientX - backgroundColorSelector.container.offsetLeft, event.clientY - backgroundColorSelector.container.offsetTop );
-}
-
-function onBackgroundColorSelectorMouseUp( event )
-{
-	window.removeEventListener('mousemove', onBackgroundColorSelectorMouseMove, false);
-	window.removeEventListener('mouseup', onBackgroundColorSelectorMouseUp, false);
-	
-	setBackgroundColor( event.clientX - backgroundColorSelector.container.offsetLeft, event.clientY - backgroundColorSelector.container.offsetTop );
-}
-
-
-function onBackgroundColorSelectorTouchStart( event )
-{
-	if(event.touches.length == 1)
-	{
-		event.preventDefault();
-		
-		setBackgroundColor( event.touches[0].pageX - backgroundColorSelector.container.offsetLeft, event.touches[0].pageY - backgroundColorSelector.container.offsetTop );
-		
-		window.addEventListener('touchmove', onBackgroundColorSelectorTouchMove, false);
-		window.addEventListener('touchend', onBackgroundColorSelectorTouchEnd, false);
-	}
-}
-
-function onBackgroundColorSelectorTouchMove( event )
-{
-	if(event.touches.length == 1)
-	{
-		event.preventDefault();
-		
-		setBackgroundColor( event.touches[0].pageX - backgroundColorSelector.container.offsetLeft, event.touches[0].pageY - backgroundColorSelector.container.offsetTop );
-	}
-}
-
-function onBackgroundColorSelectorTouchEnd( event )
-{
-	if(event.touches.length == 0)
-	{
-		event.preventDefault();
-		
-		window.removeEventListener('touchmove', onBackgroundColorSelectorTouchMove, false);
-		window.removeEventListener('touchend', onBackgroundColorSelectorTouchEnd, false);
-	}	
-}
-
-
-// MENU
-
-function onMenuForegroundColor()
-{
-	cleanPopUps();
-	
-	foregroundColorSelector.show();
-	foregroundColorSelector.container.style.left = ((SCREEN_WIDTH - foregroundColorSelector.container.offsetWidth) / 2) + 'px';
-	foregroundColorSelector.container.style.top = ((SCREEN_HEIGHT - foregroundColorSelector.container.offsetHeight) / 2) + 'px';
-
-	isForegroundColorSelectorVisible = true;
-}
-
-function onMenuBackgroundColor()
-{
-	cleanPopUps();
-
-	backgroundColorSelector.show();
-	backgroundColorSelector.container.style.left = ((SCREEN_WIDTH - backgroundColorSelector.container.offsetWidth) / 2) + 'px';
-	backgroundColorSelector.container.style.top = ((SCREEN_HEIGHT - backgroundColorSelector.container.offsetHeight) / 2) + 'px';
-
-	isBackgroundColorSelectorVisible = true;
-}
-
-function onMenuSelectorChange()
-{
-	if (BRUSHES[menu.selector.selectedIndex] == "")
-		return;
-
-	brush.destroy();
-	brush = eval("new " + BRUSHES[menu.selector.selectedIndex] + "(context)");
-
-	window.location.hash = BRUSHES[menu.selector.selectedIndex];
-}
-
-function onMenuMouseOver()
-{
-	isMenuMouseOver = true;
-}
-
-function onMenuMouseOut()
-{
-	isMenuMouseOver = false;
-}
-
-function onMenuSave()
-{
-	var context = flattenCanvas.getContext("2d");
-	
-	context.fillStyle = 'rgb(' + BACKGROUND_COLOR[0] + ', ' + BACKGROUND_COLOR[1] + ', ' + BACKGROUND_COLOR[2] + ')';
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	context.drawImage(canvas, 0, 0);
-
-	window.open(flattenCanvas.toDataURL("image/png"),'mywindow');
-}
-
-function onMenuClear()
-{
-	context.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-	brush.destroy();
-	brush = eval("new " + BRUSHES[menu.selector.selectedIndex] + "(context)");
-}
-
-function onMenuAbout()
-{
-	cleanPopUps();
-
-	isAboutVisible = true;
-	about.show();
-}
-
-
 // CANVAS
-
-
 
 function onCanvasMouseMove( event )
 {
@@ -502,78 +245,7 @@ function onCanvasMouseUp()
 }
 
 
-//
-
-function onCanvasTouchStart( event )
-{
-	cleanPopUps();		
-
-	if(event.touches.length == 1)
-	{
-		event.preventDefault();
-		
-		brush.strokeStart( event.touches[0].pageX, event.touches[0].pageY );
-		
-		window.addEventListener('touchmove', onCanvasTouchMove, false);
-		window.addEventListener('touchend', onCanvasTouchEnd, false);
-	}
-}
-
-function onCanvasTouchMove( event )
-{
-	if(event.touches.length == 1)
-	{
-		event.preventDefault();
-		brush.stroke( event.touches[0].pageX, event.touches[0].pageY );
-	}
-}
-
-function onCanvasTouchEnd( event )
-{
-	if(event.touches.length == 0)
-	{
-		event.preventDefault();
-		
-		brush.strokeEnd();
-
-		window.removeEventListener('touchmove', onCanvasTouchMove, false);
-		window.removeEventListener('touchend', onCanvasTouchEnd, false);
-	}
-}
-
-//
-
-function cleanPopUps()
-{
-	if (isForegroundColorSelectorVisible)
-	{
-		foregroundColorSelector.hide();
-		isForegroundColorSelectorVisible = false;
-	}
-		
-	if (isBackgroundColorSelectorVisible)
-	{
-		backgroundColorSelector.hide();
-		isBackgroundColorSelectorVisible = false;
-	}
-	
-	if (isAboutVisible)
-	{
-		about.hide();
-		isAboutVisible = false;
-	}
-}
-
-
-
-
-
-
-
-
 })(this,this.document);
-
-
 
 
 // this part is just the easter egg for erasure. you dont need it.
@@ -589,5 +261,4 @@ function starryEgg(){
     div.style.bottom = '250px';
     div.style.left = '50%';
     div.style.marginLeft = '-190px';
-    
 }
